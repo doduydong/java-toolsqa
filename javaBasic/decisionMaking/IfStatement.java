c97e04756e8221c7a39f92281f9d86af7a8b3699
Do Duy Dong <doduydong1310@gmail.com>
2026-01-08 20:29:55 +0700

    Decision-making

---
+++ javaBasic/decisionMaking/IfStatement.java
@@ -0,0 +1,14 @@
+package decisionMaking;
+
+public class IfStatement {
+
+    public static void main(String[] args) {
+        int number = 11;
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
